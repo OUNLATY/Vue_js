cc69e2dd15023e4842c5e7c6e9779196c9681933
LATY.OUN <LATY.OUN@PNC.LAN>
2024-05-13 17:25:16 +0700

    My homework

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <form @submit.prevent="submitForm">
+      <label for="fname">Firstname:</label>
+      <input type="text" name="fname" id="fname" v-model="fname" />
+      <label for="lname">Lastname:</label>
+      <input type="text" name="lname" id="lname" v-model="lname" />
+      <label for="style">Email:</label>
+      <input type="email" name="email" id="email" v-model="email" />
+      
+      <button type="submit">Submit</button>
+    </form>
+    <table>
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(entry, index) in entries" :key="index">
+          <td>{{ entry.fname }}</td>
+          <td>{{ entry.lname }}</td>
+          <td>{{ entry.email }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "App",
+  data() {
+    return {
+      fname: "",
+      lname: "",
+      email: "",
+      entries: [],
+    };
+  },
+  computed: {
+    fullname() {
+      return this.fname + " " + this.lname+ " " + this.email;
+    },
+  },
+  methods: {
+    submitForm() {
+      this.entries.push({
+        fname: this.fname,
+        lname: this.lname,
+        email: this.email,
+        
+        
+      });
+      this.fname = "";
+      this.lname = "";
+      this.email = "";
+      
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button{
+  background:blue;
+  color: white;
+  margin: 10px;
+  width: 5%;
+  height: 10%;
+  border-radius: 5px;
 }
 </style>
